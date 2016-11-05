3643fad52f96b4ac709e8549d3db2e40e9e23a02
naomijub <jnboeira@outlook.com>
2016-11-05 00:59:04 -0200

    Roman to Int pkg

---
+++ Converter/ToArabic/RomanToArabic.py
@@ -0,0 +1,31 @@
+'''
+Created on 04/11/2016
+
+@author: JAT
+'''
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.conv = ConvToInt()
+    
+    def tearDown(self):
+        pass
+    
+    def testInEmptyOutNone(self):
+        assert self.conv.convertRomanToInt("") == None
+        assert self.conv.convertRomanToInt(" ") == None
+    
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
+    
+class ConvToInt:
+    def __init__(self):
+        self.dic = {"I" : 1, "V" : 5, "X" : 10}
+        
+    def convertRomanToInt(self, argRoman):
+        if(argRoman == "" or argRoman == " "):
+            return None
+        
+        
