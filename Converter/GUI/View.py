58d99cc513d2fff3b9d5105675ac48a0982b26bf
naomijub <jnboeira@outlook.com>
2016-11-06 02:21:42 -0200

    Transform into class

--- Converter/GUI/View.py
+++ Converter/GUI/View.py
@@ -6,14 +6,44 @@ Created on 06/11/2016
 '''
 import tkinter as tki
 
-root = tki.Tk()
-varLabel = tki.StringVar()
-label = tki.Label(root, textvariable=varLabel, bg="#fff", width=20, anchor="e")
-varLabel.set(" ")
-label.grid(row=0, column=0, columnspan=6)
 
-varRadio = tki.IntVar()
-tki.Radiobutton(root, text="Roman", variable=varRadio, value=1).grid(row=1, column=0, columnspan=3)
-tki.Radiobutton(root, text="Arabic", variable=varRadio, value=2).grid(row=1, column=3, columnspan=3)
+#root = tki.Tk()
+#===============================================================================
+# varLabel = tki.StringVar()
+# label = tki.Label(root, textvariable=varLabel, bg="#fff", width=20, anchor="e")
+# varLabel.set(" ")
+# label.grid(row=0, column=0, columnspan=6)
+# 
+# varRadio = tki.IntVar()
+# tki.Radiobutton(root, text="Roman", variable=varRadio, value=1).grid(row=1, column=0, columnspan=3, command=sel)
+# tki.Radiobutton(root, text="Arabic", variable=varRadio, value=2).grid(row=1, column=3, columnspan=3, command=sel)
+#===============================================================================
 
-root.mainloop()  
+
+class view(tki.Tk):
+    def __init__(self, parent):
+        tki.Tk.__init__(self, parent)
+        self.parent = parent
+        self.title("RomanArabicCalculator")
+        self.run()
+        
+    def sel(self):
+        selection = str(self.varRadio.get())
+        self.varLabel.set(selection)
+    
+    def run(self):
+        self.varLabel = tki.StringVar()
+        self.label = tki.Label(self, textvariable=self.varLabel, bg="#fff", width=20, anchor="e")
+        self.varLabel.set(" ")
+        self.label.grid(row=0, column=0, columnspan=6)
+
+        self.varRadio = tki.IntVar()
+        self.varRadio.set(1)
+        Roman = tki.Radiobutton(self, text="Roman", variable=self.varRadio, value=1, command=self.sel)
+        Roman.grid(row=1, column=0, columnspan=3)
+        Arabic = tki.Radiobutton(self, text="Arabic", variable=self.varRadio, value=2, command=self.sel)
+        Arabic.grid(row=1, column=3, columnspan=3)
+    
+if __name__ == "__main__":    
+    app = view(None)
+    app.mainloop()
