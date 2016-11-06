fa2ece0a88e12747cc302aadec9016aea96be8d4
naomijub <jnboeira@outlook.com>
2016-11-06 01:38:06 -0200

    Radio buttons arabic/roman

--- Converter/GUI/View.py
+++ Converter/GUI/View.py
@@ -7,9 +7,13 @@ Created on 06/11/2016
 import tkinter as tki
 
 root = tki.Tk()
-var = tki.StringVar()
-label = tki.Label(root, textvariable=var, bg="#fff", width=20, anchor="e")
-var.set("Hello World")
+varLabel = tki.StringVar()
+label = tki.Label(root, textvariable=varLabel, bg="#fff", width=20, anchor="e")
+varLabel.set(" ")
+label.grid(row=0, column=0, columnspan=6)
 
-label.pack()
-root.mainloop()
+varRadio = tki.IntVar()
+tki.Radiobutton(root, text="Roman", variable=varRadio, value=1).grid(row=1, column=0, columnspan=3)
+tki.Radiobutton(root, text="Arabic", variable=varRadio, value=2).grid(row=1, column=3, columnspan=3)
+
+root.mainloop()  
