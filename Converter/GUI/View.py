85a8e6f1c2369ae468af1b27f4b28433d6312f2b
naomijub <jnboeira@outlook.com>
2016-11-06 22:38:53 -0200

    Added numeral buttons and lambded it to label

--- Converter/GUI/View.py
+++ Converter/GUI/View.py
@@ -10,31 +10,87 @@ class view(tki.Tk):
     def __init__(self, parent):
         tki.Tk.__init__(self, parent)
         self.parent = parent
+        self.selection = ""
+        self.arg1 = ""
+        self.arg2 = ""
+        self.operatorSymbol = ""
         self.title("RomanArabicCalculator")
         self.run()
         
-    def sel(self):
-        selection = str(self.varRadio.get())
-        self.varLabel.set(selection)
+    def sel(self, arg):
+        self.selection = self.varLabel.get() + arg
+        sel = self.selection
+        self.varLabel.set(sel)
+        
+    def typeSel(self):
+        selection = self.varRadio.get()
+        if(selection == 0):
+            pass
+        elif (selection == 1):
+            pass
+        else:
+            pass
     
     def run(self):
         self.runLabel()
         self.runRadio()
+        self.runButton()
         
     
     def runLabel(self):
+        #label 1st row
         self.varLabel = tki.StringVar()
         self.label = tki.Label(self, textvariable=self.varLabel, bg="#fff", width=20, anchor="e")
-        self.varLabel.set(" ")
         self.label.grid(row=0, column=0, columnspan=6)
         
     def runRadio(self):
+        #radio buttons 2nd row
         self.varRadio = tki.IntVar()
         self.varRadio.set(1)
-        Roman = tki.Radiobutton(self, text="Roman", variable=self.varRadio, value=1, command=self.sel)
+        Roman = tki.Radiobutton(self, text="Roman", variable=self.varRadio, value=1, command=self.typeSel)
         Roman.grid(row=1, column=0, columnspan=3)
-        Arabic = tki.Radiobutton(self, text="Arabic", variable=self.varRadio, value=2, command=self.sel)
+        Arabic = tki.Radiobutton(self, text="Arabic", variable=self.varRadio, value=2, command=self.typeSel)
         Arabic.grid(row=1, column=3, columnspan=3)
+       
+    def runButton(self):
+        #Buttons
+        #3rd row
+        self.B0 = tki.Button(self, text="0", command= lambda: self.sel('0'))
+        self.B0.grid(row=2, column=0)
+        self.B1 = tki.Button(self, text="1", command= lambda: self.sel('1'))
+        self.B1.grid(row=2, column=1)
+        self.B2 = tki.Button(self, text="2", command= lambda: self.sel('2'))
+        self.B2.grid(row=2, column=2)
+        self.B3 = tki.Button(self, text="3", command= lambda: self.sel('3'))
+        self.B3.grid(row=2, column=3)
+        self.B4 = tki.Button(self, text="4", command= lambda: self.sel('4'))
+        self.B4.grid(row=2, column=4)
+        self.B5 = tki.Button(self, text="5", command= lambda: self.sel('5'))
+        self.B5.grid(row=2, column=5)
+        #4th row
+        self.B6 = tki.Button(self, text="6", command= lambda: self.sel('6'))
+        self.B6.grid(row=3, column=0)
+        self.B7 = tki.Button(self, text="7", command= lambda: self.sel('7'))
+        self.B7.grid(row=3, column=1)
+        self.B8 = tki.Button(self, text="8", command= lambda: self.sel('8'))
+        self.B8.grid(row=3, column=2)
+        self.B9 = tki.Button(self, text="9", command= lambda: self.sel('9'))
+        self.B9.grid(row=3, column=3)
+        self.BI = tki.Button(self, text="I", command= lambda: self.sel('I'))
+        self.BI.grid(row=3, column=4)
+        self.BV = tki.Button(self, text="V", command= lambda: self.sel('V'))
+        self.BV.grid(row=3, column=5)
+        #5th row
+        self.BX = tki.Button(self, text="X", command= lambda: self.sel('X'))
+        self.BX.grid(row=4, column=0)
+        self.BL = tki.Button(self, text="L", command= lambda: self.sel('L'))
+        self.BL.grid(row=4, column=1)
+        self.BC = tki.Button(self, text="C", command= lambda: self.sel('C'))
+        self.BC.grid(row=4, column=2)
+        self.BD = tki.Button(self, text="D", command= lambda: self.sel('D'))
+        self.BD.grid(row=4, column=3)
+        self.BM = tki.Button(self, text="M", command= lambda: self.sel('M'))
+        self.BM.grid(row=4, column=4)
         
 if __name__ == "__main__":    
     app = view(None)
